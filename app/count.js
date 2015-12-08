54aa0955b511188f30b241a1932740cf3b595411
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-08 15:26:04 -0800

    work on strings and count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,13 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-
+    var counter = setInterval(function() {
+      console.log(start);
+      start++;
+      if(start > end) {
+        clearInterval(counter);
+      }
+    }, 100);
+    return {cancel : function(){clearInterval(counter)}};
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -1,13 +1,50 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
+  //reduce duplicate characters to desired minimum
   reduceString: function(str, amount) {
+    //remove char from string
+    var arr = str.split('');
+    var bucket = {};
+    var current = '';
 
+    for (var i = 0; i < arr.length; i ++) {
+
+      //reset previous bucket if current changes
+      if (current !== arr[i]) {
+        bucket[current] = undefined;
+        current = arr[i];
+      }
+
+      if (bucket[arr[i]] === undefined) {
+        bucket[arr[i]] = 1;
+        continue;
+      }
+
+      if (bucket[arr[i]] < amount) {
+        var count = bucket[arr[i]];
+        count++;
+        bucket[arr[i]] = count;
+        continue;
+      }
+
+      if (bucket[arr[i]] === amount) {
+        arr.splice(i, 1);
+        //array has been reduced by 1, so decrement index
+        //so as not to skip next one
+        i--;
+      }
+    }
+    return arr.join('');
   },
   wordWrap: function(str, cols) {
 
   },
   reverseString: function(str) {
-
+    var result = '';
+    for (var i = str.length-1; i >= 0; i--) {
+      result += str.charAt(i);
+    }
+    return result;
   }
 };
